52eb92e836dd4fca5729a13dcdd42479f62ddd10
Graham Stockton <graham.stockton@utexas.edu>
2021-08-29 13:41:41 -0500

    fixed #1, partially #2, resolved index err
    I removed the duplicate print mentioned in #1. It is now  a member
    function.
    
    I have created valid and invalid-by-inspection tests, and need now only
    to add valid-looking-but-unsolvable tests.
    
    This version also resolves an indexing error encountered during
    testing.I removed the duplicate print mentioned in #1. It is now  a
    member
    function.
    
    I have created valid and invalid-by-inspection tests, and need now only
    to add valid-looking-but-unsolvable tests.
    
    This version also resolves an indexing error encountered during testing.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <vector>
 #include <tuple>
+#include <filesystem>
+
 
 /*
 This project is a simple sudoku solver that implements recursive backtracking.
@@ -29,12 +31,15 @@ public:
             return true;
         } else {
             for (char a = '1'; a <= '9'; a++) {
-                // change in place, check if valid, wipe if not valid. If valid, call on next spot
+                // check if new value is valid
+                if (!checkPosition(newRow, newCol, a)) { continue; }
+                    
+                // plug in value and try. wipe if failes
                 problem[newRow][newCol] = a;
-                if (checkPosition(newRow, newCol)) {
-                    if (solve(newRow, newCol)) { return true; }
+                if (solve(newRow, newCol)) { 
+                    return true; 
                 } else {
-                    problem[newRow][newCol] = '.';
+                    problem[newRow][newCol] = '0';
                 }
             }
             // nothing found
@@ -51,9 +56,9 @@ public:
             for (int j = 0; j < 9; j++)
             {
                 // check box, row, and column
-                boxRow = 3*(i/3) + j/3;
+                boxRow = i - i%3 + j/3;
                 boxCol = 3*(i%3) + j%3;
-                if ('1' <= problem[boxRow][boxCol]) {
+                if ('1' <= problem[boxRow][boxCol] && problem[boxRow][boxCol] <= '9') {
                     value = 1 << (problem[boxRow][boxCol] - '0');
                     if (value & box) { return false; }
                     box |= value;
@@ -71,6 +76,32 @@ public:
             }
         }
         return true;
+    }
+
+    void print()
+    {
+        // row loop
+        for (int i = 0; i < 9; i++)
+        {
+            // vertical spacing for boxes
+            if (i % 3 == 0)
+            {
+                std::cout << std::endl;
+            }
+            // column loop
+            for (int j = 0; j < 9; j++)
+            {
+                // horizontal spacing for boxes
+                if (j % 3 == 0)
+                {
+                    std::cout << " ";
+                }
+                // print item and space
+                std::cout << problem[i][j] << "  ";
+            }
+            // end of line
+            std::cout << std::endl;
+        }
     }
 
 private:
@@ -98,92 +129,26 @@ private:
     }
 
     // checks only the row, col, box containing problem[row][col]
-    bool checkPosition(const int row, const int col)
+    bool checkPosition(const int row, const int col, const char ch)
     {
-        const char ch = problem[row][col];
-        int rowCount = 0, colCount = 0, boxCount = 0;
+        // check row, col, box
+        int boxRow = row - row%3, boxCol = col - col%3;
         for (int i = 0; i < 9; i++)
         {
-            // check row, col, box
-            if (problem[row][i] == ch) {
-                colCount++;
-            } 
-            if (problem[i][col] == ch) {
-                rowCount++;
-            }
-            if (problem[3*(row/3) + i/3][3*(col/3) + i%3] == ch) {
-                boxCount++;
-            }
-
-            // check if too many values
-            if (rowCount > 1 || colCount > 1 || boxCount > 1) {
-                return false;
-            }
+            if (problem[row][i] == ch) { return false; }
+            if (problem[i][col] == ch) { return false; }
+            if (problem[boxRow + i/3][boxCol + i%3] == ch) { return false; }
         }
-
+        //
         return true;
-    }
-
-    void print(std::vector<std::vector<char>> &sudoku)
-    {
-        // row loop
-        for (int i = 0; i < 9; i++)
-        {
-            // vertical spacing for boxes
-            if (i % 3 == 0)
-            {
-                std::cout << std::endl;
-            }
-
-            // column loop
-            for (int j = 0; j < 9; j++)
-            {
-                // horizontal spacing for boxes
-                if (j % 3 == 0)
-                {
-                    std::cout << "  ";
-                }
-                // print item and space
-                std::cout << sudoku[i][j] << "  ";
-            }
-            // end of line
-            std::cout << std::endl << std::endl;
-        }
     }
 };
 
-void print(std::vector<std::vector<char>> &sudoku)
-{
-    // row loop
-    for (int i = 0; i < 9; i++)
-    {
-        // vertical spacing for boxes
-        if (i % 3 == 0)
-        {
-            std::cout << std::endl;
-        }
-
-        // column loop
-        for (int j = 0; j < 9; j++)
-        {
-            // horizontal spacing for boxes
-            if (j % 3 == 0)
-            {
-                std::cout << "  ";
-            }
-            // print item and space
-            std::cout << sudoku[i][j] << "  ";
-        }
-        // end of line
-        std::cout << std::endl << std::endl;
-    }
-}
-
-int main()
+std::vector<std::vector<char>> readFile(std::string filename)
 {
     // read in file
     std::ifstream file;
-    file.open("sudoku.in");
+    file.open(filename);
 
     // create board vec<vec>
     std::vector<std::vector<char>> sudoku;
@@ -200,23 +165,56 @@ int main()
         }
         sudoku.push_back(row);
     }
+    //
+    return sudoku;
+}
+
+// test -- should work with windows and linux. Requires C++ 17
+void test() 
+{
+    std::cout << "\nStarting Tests" << std::endl;
+
+    std::string path = "tests";
+    for (const auto& file : std::filesystem::directory_iterator(path))
+    {
+        std::vector<std::vector<char>> sudoku;
+        sudoku = readFile(file.path().string());
+        sudokuSolver s(sudoku);
+
+        if (!s.checkBoard()) {
+            std::cout << file.path() << ": Invalid" << std::endl;
+        } else if (s.solve()) {
+            std::cout << file.path() << ": Solved" << std::endl;
+        } else {
+            std::cout << file.path() << ": No Solution" << std::endl;
+        }
+    }
+}
+
+int main()
+{
+    // read in problem
+    std::vector<std::vector<char>> sudoku = readFile("sudoku.in");
 
     // print the problem
     std::cout << "Starting problem:" << std::endl;
-    print(sudoku);
 
     // create and solve
     sudokuSolver s(sudoku);
+    s.print();
     if (!s.checkBoard()) {
         std::cout << "Sudoku input has an invalid position" << std::endl;
         return -1;
     } else if (s.solve()) {
-        std::cout << "Solution:" << std::endl;
-        print(s.problem);
+        std::cout << "\nSolution:" << std::endl;
+        s.print();
     } else {
-        std::cout << "No solution found :(" << std::endl;
+        std::cout << "No solution" << std::endl;
         return -1;
     }
+
+    // run tests
+    test();
 
     //
     return 0;
